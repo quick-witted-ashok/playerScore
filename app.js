f584c0a6e91cd6d02b46ae414fcc3825c9317f94
quick-witted-ashok <124670515+quick-witted-ashok@users.noreply.github.com>
2023-12-15 15:15:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,33 +27,101 @@ const initializeDBAndServer = async () => {
 
 initializeDBAndServer();
 
-const convert = (stats) => {
-  return {
-    stateName: stats.state_name,
-  };
-};
-
-const convertState = (each) => {
+const convertFont = (each) => {
   return {
     playerId: each.player_id,
     playerName: each.player_name,
   };
 };
 
-const convertDistrict = (movie) => {
+const convertMatch = (each) => {
   return {
-    matchId: movie.match_id,
-    match: movie.match,
-    year: movie.year,
+    matchId: each.match_id,
+    match: each.match,
+    year: each.year,
   };
 };
 
+const convert = (each) => {
+  return {
+    playerId: each.playerId,
+    playerName: each.playerName,
+    totalScore: each.totalScore,
+    totalFours: each.totalFours,
+    totalSixes: each.totalSixes,
+  };
+};
 app.get("/players/", async (request, response) => {
-  const getStateQuery = `
-    SELECT
-      *
-    FROM
-      player_details;`;
-  const statesArray = await db.all(getStateQuery);
-  response.send(statesArray.map((eachState) => convertState(eachState)));
+  const query = `SELECT * FROM player_details`;
+  const result = await db.all(query);
+  response.send(result.map((eachPlayer) => convertFont(eachPlayer)));
 });
+
+app.get("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const players = `SELECT * FROM player_details WHERE player_id=${playerId}`;
+  const result = await db.get(players);
+  response.send(convertFont(result));
+});
+
+app.put("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const playerDetails = request.body;
+  const { playerName } = playerDetails;
+  const query = `UPDATE player_details 
+                    SET player_name = '${playerName}'
+                    WHERE player_id =${playerId}`;
+
+  const result = await db.run(query);
+  response.send("Player Details Updated");
+});
+
+app.get("/matches/:matchId/", async (request, response) => {
+  const { matchId } = request.params;
+  const query = `SELECT * FROM match_details WHERE match_id = ${matchId}`;
+  const result = await db.get(query);
+
+  response.send(convertMatch(result));
+});
+
+app.get("/players/:playerId/matches/", async (request, response) => {
+  const { playerId } = request.params;
+  const query = `SELECT * FROM match_details
+                 WHERE match_id = (SELECT match_id 
+                                    FROM player_match_score 
+                                    WHERE player_id = ${playerId})`;
+  const result = await db.all(query);
+  response.send(result.map((eachMatch) => convertMatch(eachMatch)));
+});
+
+app.get("/matches/:matchId/players/", async (request, response) => {
+  const { matchId } = request.params;
+  const query = `SELECT * FROM player_details
+                        WHERE player_id = (SELECT player_id
+                                            FROM player_match_score
+                                            WHERE match_id =${matchId})`;
+
+  const result = await db.all(query);
+  response.send(result.map((eachPlayer) => convertFont(eachPlayer)));
+});
+
+app.get("/players/:playerId/playerScores", async (request, response) => {
+  const { playerId } = request.params;
+
+  const query = `
+    SELECT
+    player_details.player_id AS playerId,
+    player_details.player_name AS playerName,
+    SUM(player_match_score.score) AS totalScore,
+    SUM(fours) AS totalFours,
+    SUM(sixes) AS totalSixes FROM 
+    player_details INNER JOIN player_match_score ON
+    player_details.player_id = player_match_score.player_id
+    WHERE player_details.player_id = ${playerId};
+    `;
+  const result = await db.get(query);
+  console.log(result.playerId);
+  response.send(convert(result));
+});
+
+module.exports = app;
